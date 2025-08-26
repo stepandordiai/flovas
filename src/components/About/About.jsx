f8aa8ff1ce9a4f01ee68f791a5a999c41e1703ea
Stepan Dordiai <stepandordiai@gmail.com>
2025-08-26 14:39:16 +0200

    Update Contacts and About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -19,78 +19,73 @@ const About = ({ placesData }) => {
 
 	return (
 		<div className="about" id="about">
-			<h2 className="about__title">{t("about_title")} flovas s.r.o.</h2>
-			<p className="about__desc">
-				flovas s.r.o. — це кадрова агенція, яка спеціалізується на
-				працевлаштуванні по всій території Чехії. Ми пропонуємо роботу в різних
-				галузях — виробництво, логістика, будівництво, складські процеси та інші
-				сфери.
-			</p>
+			<h2 className="about__title">{t("about_title")} FLOVAS s.r.o.</h2>
+			<p className="about__desc">{t("about.desc")}</p>
 			<h3 className="about__benefits-title">
 				{t("about.our_advantages_title")}
 			</h3>
 			<ul className="about__benefits-list">
 				<li className="about__benefits-item">
-					<img src={mapIcon} width={50} alt="" loading="lazy" />
+					<img src={mapIcon} width={50} height={50} alt="" />
 					<span>{t("about.our_advantages1")}</span>
 				</li>
 				<li className="about__benefits-item">
-					<img src={peopleIcon} width={50} alt="" loading="lazy" />
+					<img src={peopleIcon} width={50} height={50} alt="" />
 					<span>{t("about.our_advantages2")}</span>
 				</li>
 				<li className="about__benefits-item">
-					<img src={moneyIcon} width={50} alt="" loading="lazy" />
+					<img src={moneyIcon} width={50} height={50} alt="" />
 					<span>{t("about.our_advantages3")}</span>
 				</li>
 				<li className="about__benefits-item">
-					<img src={accommodationIcon} width={50} alt="" loading="lazy" />
+					<img src={accommodationIcon} width={50} height={50} alt="" />
 					<span>{t("about.our_advantages4")}</span>
 				</li>
 				<li className="about__benefits-item">
-					<img src={hiringIcon} width={50} alt="" loading="lazy" />
+					<img src={hiringIcon} width={50} height={50} alt="" />
 					<span>{t("about.our_advantages5")}</span>
 				</li>
 				<li className="about__benefits-item">
-					<img src={documentsIcon} width={50} alt="" loading="lazy" />
+					<img src={documentsIcon} width={50} height={50} alt="" />
 					<span>{t("about.our_advantages6")}</span>
 				</li>
 				<li className="about__benefits-item">
-					<img src={insuranceIcon} width={50} alt="" loading="lazy" />
+					<img src={insuranceIcon} width={50} height={50} alt="" />
 					<span>{t("about.our_advantages7")}</span>
 				</li>
 				<li className="about__benefits-item">
-					<img src={freeIcon} width={50} alt="" loading="lazy" />
+					<img src={freeIcon} width={50} height={50} alt="" />
 					<span>{t("about.our_advantages9")}</span>
 				</li>
 				<li className="about__benefits-item">
-					<img src={hourIcon} width={50} alt="" loading="lazy" />
+					<img src={hourIcon} width={50} height={50} alt="" />
 					<span>{t("about.our_advantages10")}</span>
 				</li>
 				<li className="about__benefits-item">
-					<img src={busIcon} width={50} alt="" loading="lazy" />
+					<img src={busIcon} width={50} height={50} alt="" />
 					<span>{t("about.our_advantages11")}</span>
 				</li>
 				<li className="about__benefits-item">
-					<img src={supportIcon} width={50} alt="" loading="lazy" />
+					<img src={supportIcon} width={50} height={50} alt="" />
 					<span>{t("about.our_advantages12")}</span>
 				</li>
 				<li className="about__benefits-item">
-					<img src={misunderstandingIcon} width={50} alt="" loading="lazy" />
+					<img src={misunderstandingIcon} width={50} height={50} alt="" />
 					<span>{t("about.our_advantages13")}</span>
 				</li>
 				<li className="about__benefits-item">
-					<img src={movingIcon} width={50} alt="" loading="lazy" />
+					<img src={movingIcon} width={50} height={50} alt="" />
 					<span>{t("about.our_advantages14")}</span>
 				</li>
 			</ul>
-			<h3 className="about__goal-title">Наша мета</h3>
-			<p className="about__goal-desc">
-				Забезпечити кожному клієнту стабільну роботу, комфортні умови проживання
-				та повну юридичну підтримку для впевненого старту в Чехії.
-			</p>
-			<h3 className="about__places-title">Працюємо по всій Чехії</h3>
+			<h3 className="about__goal-title">{t("about.goal_title")}</h3>
+			<p className="about__goal-desc">{t("about.goal_desc")}</p>
+			<h3 className="about__places-title">
+				{t("about.employment_place_title")}
+			</h3>
 			<p className="about__places-desc">
-				{placesData.map((place) => t(place)).join(", ")} та інші міста.
+				{placesData.map((place) => t(place)).join(", ")}{" "}
+				{t("about.employment_place_desc")}
 			</p>
 		</div>
 	);
