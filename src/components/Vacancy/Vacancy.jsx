b1e61868989b1b5b30fd255b332d85d5391f29ec
Stepan Dordiai <stepandordiai@gmail.com>
2025-09-03 14:04:22 +0200

    Update Vacancy component

--- src/components/Vacancy/Vacancy.jsx
+++ src/components/Vacancy/Vacancy.jsx
@@ -8,41 +8,37 @@ const Vacancy = ({ vacancy, handleTelFormBanner }) => {
 	const { _id, img, place, title, updatedAt, isActive } = vacancy;
 
 	return (
-		<>
-			<div className="vacancy">
-				{img ? (
-					<img src={img} alt={title} loading="lazy" />
+		<div className="vacancy">
+			{img ? (
+				<img src={img} alt={title} loading="lazy" />
+			) : (
+				<div className="vacancy__no-img"></div>
+			)}
+			<div className="vacancy__details">
+				{isActive ? (
+					<p className="vacancy__active">
+						<span></span> <span>{t("active_vacancy")}</span>
+					</p>
 				) : (
-					<div className="vacancy__no-img"></div>
+					<p className="vacancy__inactive">
+						<span></span> <span>{t("inactive_vacancy")}</span>
+					</p>
 				)}
-				<div className="vacancy__details">
-					{isActive ? (
-						<p className="vacancy__active">
-							<span></span> <span>{t("active_vacancy")}</span>
-						</p>
-					) : (
-						<p className="vacancy__inactive">
-							<span></span> <span>{t("inactive_vacancy")}</span>
-						</p>
-					)}
-					<p className="vacancy__date">
-						Опубліковано: {updatedAt.slice(0, 10)}
-					</p>
-					<p>
-						<span style={{ fontWeight: 500 }}>Місто</span>: {place}
-					</p>
-					<p className="vacancy__title">{title}</p>
-				</div>
-				<div className="vacancy__link-container">
-					<NavLink className={"vacancy__link"} to={`/vacancy-page/${_id}`}>
-						{t("more_info_btn")}
-					</NavLink>
-					<button onClick={handleTelFormBanner} className="vacancy__btn">
-						{t("contact_us_title")}
-					</button>
-				</div>
+				<p className="vacancy__date">Опубліковано: {updatedAt.slice(0, 10)}</p>
+				<p>
+					<span style={{ fontWeight: 500 }}>Місто</span>: {place}
+				</p>
+				<p className="vacancy__title">{title}</p>
 			</div>
-		</>
+			<div className="vacancy__link-container">
+				<NavLink className={"vacancy__link"} to={`/vacancy-page/${_id}`}>
+					{t("more_info_btn")}
+				</NavLink>
+				<button onClick={handleTelFormBanner} className="vacancy__btn">
+					{t("contact_us_title")}
+				</button>
+			</div>
+		</div>
 	);
 };
 
