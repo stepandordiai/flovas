bff0ab1f7f7c891500fd7cbae7ca90535ec2234c
Stepan Dordiai <stepandordiai@gmail.com>
2025-09-16 15:12:08 +0200

    Update FloatingContact

--- src/components/FloatingContact/FloatingContact.jsx
+++ src/components/FloatingContact/FloatingContact.jsx
@@ -1,32 +1,73 @@
-import phoneIcon from "/icons/phone.png";
+import viberIcon from "/icons/viber.png";
 import whatsappIcon from "/icons/whatsapp.png";
 import telegramIcon from "/icons/telegram.png";
+import messageIcon from "/icons/message.png";
+import { useState } from "react";
 import "./FloatingContact.scss";
 
 const FloatingContact = () => {
+	const [isActive, setIsActive] = useState(false);
+
+	const handleFloatingContact = () => {
+		setIsActive((prev) => !prev);
+	};
+
 	return (
 		<div className="floating-contact">
-			<a href="https://t.me/flovas_agency" target="_blank">
-				<img
-					title="Telegram"
-					width={50}
-					height={50}
-					src={telegramIcon}
-					alt="Telegram"
-				/>
-			</a>
-			<a href="https://wa.me/420777957290" target="_blank">
-				<img
-					title="WhatsApp"
-					width={50}
-					height={50}
-					src={whatsappIcon}
-					alt="WhatsApp"
-				/>
-			</a>
-			<a href="tel:+420777957290">
-				<img title="Phone" width={50} height={50} src={phoneIcon} alt="Phone" />
-			</a>
+			<div
+				className={
+					isActive
+						? "floating-contact-container floating-contact-container--active"
+						: "floating-contact-container"
+				}
+			>
+				<div
+					className={
+						isActive
+							? "floating-contact-container-inner floating-contact-container-inner--active"
+							: "floating-contact-container-inner"
+					}
+				>
+					<a
+						href="https://t.me/flovas_agency"
+						target="_blank"
+						data-value="Telegram"
+					>
+						<img
+							title="Telegram"
+							width={40}
+							height={40}
+							src={telegramIcon}
+							alt="Telegram"
+						/>
+					</a>
+					<a
+						href="https://wa.me/420777957290"
+						target="_blank"
+						data-value="WhatsApp"
+					>
+						<img
+							title="WhatsApp"
+							width={40}
+							height={40}
+							src={whatsappIcon}
+							alt="WhatsApp"
+						/>
+					</a>
+					<a href="viber://chat?number=+420777957290" data-value="Viber">
+						<img
+							title="Viber"
+							width={40}
+							height={40}
+							src={viberIcon}
+							alt="Viber"
+						/>
+					</a>
+				</div>
+			</div>
+			<button className="floating-contact__btn" onClick={handleFloatingContact}>
+				<img src={messageIcon} width={40} height={40} alt="" />
+			</button>
 		</div>
 	);
 };
