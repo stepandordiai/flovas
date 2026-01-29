ff12f2a3e0823a72aed456a0f4bdf288b84fc876
Stepan Dordiai <stepandordiai@gmail.com>
2026-01-29 10:32:27 +0100

    refactor: update sitemap

--- frontend/src/app/sitemap.ts
+++ frontend/src/app/sitemap.ts
@@ -1,24 +1,37 @@
 import type { MetadataRoute } from "next";
+import vacanciesData from "./lib/data/vacancies-data.json";
+import { VacancyInterface } from "./interfaces/Vacancy";
 
+const vacancies: VacancyInterface[] = vacanciesData;
 const BASE_URL = "https://www.flovas.cz";
 const locales = ["uk", "cs", "sk", "en"] as const;
 
 export default function sitemap(): MetadataRoute.Sitemap {
 	const now = new Date();
 
-	const alternates: Record<string, string> = {
-		uk: `${BASE_URL}/uk`,
-		cs: `${BASE_URL}/cs`,
-		sk: `${BASE_URL}/sk`,
-		en: `${BASE_URL}/en`,
-		"x-default": `${BASE_URL}/uk`,
-	};
+	// TODO: LEARN THIS
+	const alternates = (path: string) =>
+		Object.fromEntries(
+			locales.map((locale) => [locale, `${BASE_URL}/${locale}${path}`]),
+		);
 
-	return locales.map((locale) => ({
+	const localePages = locales.map((locale) => ({
 		url: `${BASE_URL}/${locale}`,
 		lastModified: now,
 		alternates: {
-			languages: alternates,
+			languages: alternates(""),
 		},
 	}));
+
+	const vacanciesLocalePages = locales.flatMap((locale) =>
+		vacancies.map((vacancy) => ({
+			url: `${BASE_URL}/${locale}/${vacancy.id}`,
+			lastModified: now,
+			alternates: {
+				languages: alternates(`/${vacancy.id}`),
+			},
+		})),
+	);
+
+	return [...localePages, ...vacanciesLocalePages];
 }
