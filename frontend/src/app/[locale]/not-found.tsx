06ec89a724a90e3803bef7121f2ec247d0ac858a
Stepan Dordiai <stepandordiai@gmail.com>
2026-04-14 15:49:29 +0200

    feat: update not-found page

--- frontend/src/app/[locale]/not-found.tsx
+++ frontend/src/app/[locale]/not-found.tsx
@@ -1,13 +1,34 @@
-import Link from "next/link";
+// import Link from "next/link";
+import { Link } from "@/i18n/navigation";
 
 export default function NotFound() {
 	return (
-		<div className="not-found">
-			<span style={{ fontWeight: 500, fontSize: "3rem" }}>404</span>
-			<span>Сторінку не знайдено</span>
-			<Link className="not-found__link" href="/">
-				Повернутись на головну
-			</Link>
-		</div>
+		<main className="not-found">
+			<span style={{ fontWeight: 500, fontSize: "clamp(6rem, 16vw, 12rem)" }}>
+				404
+			</span>
+			<h1 style={{ fontWeight: 500, fontSize: "clamp(1rem, 2vw, 2rem)" }}>
+				Сторінку не знайдено
+			</h1>
+			<p style={{ textAlign: "center" }}>
+				На жаль, сторінка, яку ви шукаєте, не існує або була переміщена.
+				Скористайтеся меню або поверніться на головну сторінку.
+			</p>
+			<div
+				style={{
+					display: "flex",
+					justifyContent: "center",
+					flexDirection: "column",
+					gap: 5,
+				}}
+			>
+				<Link className="not-found__link" href="/">
+					Повернутись на головну
+				</Link>
+				<Link className="not-found__link" href="/prace">
+					Вакансії
+				</Link>
+			</div>
+		</main>
 	);
 }
