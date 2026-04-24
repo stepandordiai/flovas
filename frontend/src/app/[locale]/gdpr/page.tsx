cec8c208f75c3016b378fe8c450976dd72c69c95
Stepan Dordiai <stepandordiai@gmail.com>
2026-04-24 11:40:07 +0200

    feat: ...

--- frontend/src/app/[locale]/gdpr/page.tsx
+++ frontend/src/app/[locale]/gdpr/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
 import { routing } from "@/i18n/routing";
 import Breadcrumbs from "@/components/common/Breadcrumbs/Breadcrumbs";
 import ScrollToTopBtn from "@/components/ScrollToTopBtn/ScrollToTopBtn";
@@ -31,18 +32,18 @@ export async function generateMetadata({
 	};
 }
 
-const Gdpr = () => {
+export default async function Gdpr() {
+	const t = await getTranslations();
+
 	return (
 		<main className={`main ${styles.gdpr}`}>
 			<Breadcrumbs links={[{ label: "Zásady ochrany osobních údajů" }]} />
 			<div className={styles.container}>
-				<h1 className={styles["gdpr__title"]}>
-					Zásady ochrany osobních údajů (GDPR)
-				</h1>
+				<h1 className={styles["gdpr__title"]}>{t("privacyPolicy.title")}</h1>
 				<div>
-					<p>Společnost: FLOVAS s.r.o.</p>
+					<p>{t("privacyPolicy.company")} FLOVAS s.r.o.</p>
 					<p>
-						Sídlo:{" "}
+						{t("privacyPolicy.address")}{" "}
 						<a href="https://maps.app.goo.gl/YsdJ4Lm1hQqYPuyt8" target="_blank">
 							Pod Hroby 271 Kolín IV
 						</a>
@@ -51,113 +52,64 @@ const Gdpr = () => {
 						IČO: <CopyBtn />
 					</p>
 				</div>
-				<p>
-					Tyto zásady vysvětlují, jakým způsobem shromažďujeme, používáme a
-					chráníme vaše osobní údaje v rámci zprostředkování zaměstnání a
-					personální agendy.
-				</p>
+				<p>{t("privacyPolicy.txt1")}</p>
 				<ol className={styles.ol}>
 					<li>
-						Kdo je správcem vašich údajů?
-						<p>
-							Správcem osobních údajů je společnost FLOVAS s.r.o. Odpovídáme za
-							to, aby vaše údaje byly zpracovávány zákonně, transparentně a
-							bezpečně v souladu s nařízením EU 2016/679 (GDPR).
-						</p>
+						{t("privacyPolicy.txt2")}
+						<p>{t("privacyPolicy.txt3")}</p>
 					</li>
 
 					<li>
-						Jaké údaje shromažďujeme?
-						<p>
-							Pro účely nalezení vhodné pracovní pozice a uzavření
-							pracovněprávního vztahu zpracováváme zejména:
-						</p>
+						{t("privacyPolicy.txt4")}
+						<p>{t("privacyPolicy.txt5")}</p>
 						<ul className={styles.ul}>
-							<li>
-								Identifikační údaje: jméno, příjmení, datum narození, státní
-								občanství.
-							</li>
-							<li>
-								Kontaktní údaje: telefonní číslo, e-mail, adresa bydliště.
-							</li>
-							<li>
-								Profesní údaje: životopis (CV), informace o vzdělání,
-								předchozích pracovních zkušenostech a dovednostech.
-							</li>
-							<li>
-								Doklady: kopie víz, povolení k pobytu, údaje z pasu (nezbytné
-								pro zákonnou registraci zaměstnance).
-							</li>
+							<li>{t("privacyPolicy.txt6")}</li>
+							<li>{t("privacyPolicy.txt7")}</li>
+							<li>{t("privacyPolicy.txt8")}</li>
+							<li>{t("privacyPolicy.txt9")}</li>
 						</ul>
 					</li>
 					<li>
-						Účel a právní základ zpracování
-						<p>Vaše údaje zpracováváme na základě těchto právních titulů:</p>
+						{t("privacyPolicy.txt10")}
+						<p>{t("privacyPolicy.txt11")}</p>
 						<ol className={styles["ol-inner"]}>
-							<li>
-								Plnění smlouvy či provedení opatření před uzavřením smlouvy:
-								abychom vám mohli zprostředkovat práci a uzavřít smlouvu.
-							</li>
-							<li>
-								Plnění právní povinnosti: hlášení na úřady (Úřad práce, OSSZ,
-								zdravotní pojišťovny) a daňová agenda.
-							</li>
-							<li>
-								Souhlas: pro uchování v databázi uchazečů pro budoucí pracovní
-								nabídky.
-							</li>
-							<li>
-								Oprávněný zájem: pro ochranu našich práv v případě právních
-								sporů.
-							</li>
+							<li>{t("privacyPolicy.txt12")}</li>
+							<li>{t("privacyPolicy.txt13")}</li>
+							<li>{t("privacyPolicy.txt14")}</li>
+							<li>{t("privacyPolicy.txt15")}</li>
 						</ol>
 					</li>
 					<li>
-						Komu vaše údaje předáváme?
-						<p>Vaše údaje mohou být poskytnuty:</p>
+						{t("privacyPolicy.txt16")}
+						<p>{t("privacyPolicy.txt17")}</p>
 						<ul className={styles.ul}>
-							<li>
-								Zaměstnavatelům (klientům): potenciálním firmám, u kterých se
-								ucházíte o práci.
-							</li>
-							<li>
-								Státním orgánům: Úřad práce ČR, Celní správa, zdravotní
-								pojišťovny a správa sociálního zabezpečení.
-							</li>
-							<li>
-								Zpracovatelům: externím účetním a mzdovým firmám či
-								poskytovatelům IT služeb.
-							</li>
+							<li>{t("privacyPolicy.txt18")}</li>
+							<li>{t("privacyPolicy.txt19")}</li>
+							<li>{t("privacyPolicy.txt20")}</li>
 						</ul>
 					</li>
 					<li>
-						Doba uchovávání údajů
+						{t("privacyPolicy.txt21")}
 						<ul className={styles.ul}>
-							<li>Po dobu výběrového řízení na konkrétní pozici.</li>
-							<li>
-								Po dobu trvání pracovního poměru a následně po dobu 10 let (dle
-								archivačních lhůt daných českými zákony).
-							</li>
-							<li>
-								3 roky na základě vašeho souhlasu (pro účely budoucích nabídek),
-								pokud souhlas neodvoláte dříve.
-							</li>
+							<li>{t("privacyPolicy.txt22")}</li>
+							<li>{t("privacyPolicy.txt23")}</li>
+							<li>{t("privacyPolicy.txt24")}</li>
 						</ul>
 					</li>
 					<li>
-						Vaše práva
-						<p>Máte právo:</p>
+						{t("privacyPolicy.txt25")}
+						<p>{t("privacyPolicy.txt26")}</p>
 						<ul className={styles.ul}>
-							<li>Požadovat přístup ke svým osobním údajům.</li>
-							<li>Na opravu neúplných či nesprávných údajů.</li>
-							<li>Na výmaz údajů („právo být zapomenut“).</li>
-							<li>Kdykoliv odvolat svůj souhlas se zpracováním.</li>
-							<li>Podat stížnost u Úřadu pro ochranu osobních údajů (ÚOOÚ).</li>
+							<li>{t("privacyPolicy.txt27")}</li>
+							<li>{t("privacyPolicy.txt28")}</li>
+							<li>{t("privacyPolicy.txt29")}</li>
+							<li>{t("privacyPolicy.txt30")}</li>
+							<li>{t("privacyPolicy.txt31")}</li>
 						</ul>
 					</li>
 					<li>
-						Kontakt
-						<p>V případě dotazů ohledně GDPR nás můžete kontaktovat:</p>
+						{t("privacyPolicy.contact")}
+						<p>{t("privacyPolicy.txt32")}</p>
 						<ul className={styles.ul}>
 							<li>
 								Email: <a href="mailto:info@neresen.cz">info@neresen.cz</a>
@@ -172,6 +124,4 @@ const Gdpr = () => {
 			<ScrollToTopBtn />
 		</main>
 	);
-};
-
-export default Gdpr;
+}
