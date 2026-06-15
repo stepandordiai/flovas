c4c9b03a7ae2649f7b389079774d8d6b456b46c3
Stepan Dordiai <stepandordiai@gmail.com>
2026-06-15 14:32:47 +0200

    feat: update Sidebar

--- admin/src/components/layout/Sidebar/Sidebar.tsx
+++ admin/src/components/layout/Sidebar/Sidebar.tsx
@@ -1,67 +1,65 @@
 import { NavLink } from "react-router-dom";
 import HouseIcon from "../../icons/HouseIcon";
-import { useState } from "react";
 import PeopleIcon from "../../icons/PeopleIcon";
 import CollectionIcon from "../../icons/CollectionIcon";
-import CollapseIcon from "../../icons/CollapseIcon";
 import { supabase } from "../../../lib/supabase";
 import "./styles.scss";
 
 const Sidebar = () => {
-	const [collapsed, setCollapsed] = useState(false);
-
 	// TODO: LEARN THIS
 	const handleLogout = async () => {
 		await supabase.auth.signOut();
 	};
 
 	return (
-		<aside className={`sidebar ${collapsed ? "sidebar--collapsed" : ""}`}>
-			<div className="sidebar-top">
-				<NavLink className="sidebar__logo" to="/">
-					<img src="/icon.svg" width={50} alt="" />
-					<span className={collapsed ? "label" : ""}>flovas (admin)</span>
-				</NavLink>
-				<button
-					className="sidebar__collapse-btn"
-					onClick={() => setCollapsed((prev) => !prev)}
-				>
-					<CollapseIcon />
-				</button>
-			</div>
+		<aside className="sidebar">
+			<NavLink className="sidebar__logo" to="/">
+				<img src="/icon.svg" width={50} alt="" />
+				<span>flovas (admin)</span>
+			</NavLink>
 			<nav className="sidebar-nav">
 				<NavLink
 					className={({ isActive }) =>
 						`sidebar-nav__link ${isActive ? "sidebar-nav__link--active" : ""}`
 					}
-					to={"/"}
+					to="/"
 				>
 					<HouseIcon />
-					<span className={collapsed ? "label" : ""}>Головна</span>
+					<span>Головна</span>
 				</NavLink>
 				<NavLink
 					className={({ isActive }) =>
 						`sidebar-nav__link ${isActive ? "sidebar-nav__link--active" : ""}`
 					}
-					to={"/vacancies"}
+					to="/vacancies"
 				>
 					<CollectionIcon />
-					<span className={collapsed ? "label" : ""}>Вакансії</span>
+					<span>Вакансії</span>
 				</NavLink>
 				<NavLink
 					className={({ isActive }) =>
 						`sidebar-nav__link ${isActive ? "sidebar-nav__link--active" : ""}`
 					}
-					to={"/leads"}
+					to="/leads"
 				>
 					<PeopleIcon />
-					<span className={collapsed ? "label" : ""}>Ліди</span>
+					<span>Ліди</span>
 				</NavLink>
 			</nav>
 
 			<button className="logout-btn" onClick={handleLogout}>
 				Вийти
 			</button>
+			<p>
+				by{" "}
+				<a
+					href="https://www.heeeyooo.com/en"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					heeeyooo studio
+				</a>
+			</p>
 		</aside>
 	);
 };
