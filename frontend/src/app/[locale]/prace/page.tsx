12b7be13f28f1866b34c2e411acb6d5dd4935e28
Stepan Dordiai <stepandordiai@gmail.com>
2026-06-27 13:52:13 +0200

    feat: update prace page with ItemList JSON-LD

--- frontend/src/app/[locale]/prace/page.tsx
+++ frontend/src/app/[locale]/prace/page.tsx
@@ -1,8 +1,9 @@
-import type { Metadata } from "next";
+import { Metadata } from "next";
 import { getTranslations } from "next-intl/server";
 import { routing } from "@/i18n/routing";
 import VacanciesClient from "./VacanciesClient";
 import { getVacancies } from "@/services/vacancies";
+import { BASE_URL } from "@/lib/constants";
 import "./Vacancies.scss";
 
 export async function generateMetadata({
@@ -39,17 +40,21 @@ export default async function Vacancies({
 	const t = await getTranslations({ locale });
 
 	const { data, error } = await getVacancies();
+	if (error) console.error("Failed to load vacancies:", error.message);
 	const vacancies = data ?? [];
 
+	// TODO: learn this
+	// ItemList
 	const jsonLd = {
 		"@context": "https://schema.org",
 		"@type": "ItemList",
 		name: t("vacancies_title"),
-		url: `https://www.flovas.cz/${locale}/prace`,
+		url: `${BASE_URL}/${locale}/prace`,
 		itemListElement: vacancies.map((vacancy, i) => ({
 			"@type": "ListItem",
 			position: i + 1,
-			url: `https://www.flovas.cz/${locale}/prace/${vacancy.id}`,
+			url: `${BASE_URL}/${locale}/prace/${vacancy.id}`,
+			name: vacancy.title,
 		})),
 	};
 
