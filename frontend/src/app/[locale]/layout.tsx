0ae6af6e32bee15aa867ad3bcbf89840d18af0fd
Stepan Dordiai <stepandordiai@gmail.com>
2026-06-27 13:46:24 +0200

    feat: update layout with EmploymentAgency JSON-LD

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -54,26 +54,41 @@ export default async function LocaleLayout({
 }: Readonly<LocaleLayoutProps>) {
 	const { locale } = await params;
 
-	const t = await getTranslations({ locale });
-
 	if (!hasLocale(routing.locales, locale)) {
 		notFound();
 	}
+
+	const t = await getTranslations({ locale });
+
+	// TODO: learn this
+	// EmploymentAgency
+	const jsonLd = {
+		"@context": "https://schema.org",
+		"@type": "EmploymentAgency",
+		name: "flovas",
+		url: `${BASE_URL}/${locale}`,
+		description: t("about.desc"),
+		logo: `${BASE_URL}/flovas-og-c.png`,
+		telephone: "+420777957290",
+		address: {
+			"@type": "PostalAddress",
+			streetAddress: "Pod Hroby 271",
+			addressLocality: "Kolín",
+			addressCountry: "CZ",
+		},
+		sameAs: [
+			"https://www.facebook.com/flovas.agentura/",
+			"https://www.instagram.com/flovas.cz/",
+			"https://www.tiktok.com/@flovas.cz",
+		],
+	};
 
 	return (
 		<html lang={locale}>
 			<head>
 				<script
 					type="application/ld+json"
-					dangerouslySetInnerHTML={{
-						__html: JSON.stringify({
-							"@context": "https://schema.org",
-							"@type": "EmploymentAgency",
-							name: "flovas",
-							url: `https://www.flovas.cz/${locale}`,
-							description: t("home.meta.description"),
-						}),
-					}}
+					dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
 				/>
 			</head>
 			<body className={montserrat.variable}>
